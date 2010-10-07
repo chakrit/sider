b9b7cde6fb75d57283a936a46dff301b52420e04
Chakrit Wichian <chakrit@2nitedesign.com>
2010-10-07 15:03:29 +0700

    Fix nuspec so its compatible with http://nupackpackages.codeplex.com/

--- package.bat
+++ package.bat
@@ -6,6 +6,12 @@ msbuild /p:Configuration=Package Sider.sln
 echo Building sider.all.zip...
 .\tools\7za.exe a -r sider.all.zip build\*.*
 
+echo Build NuPkg?
+choice
+if errorlevel 2 goto end
+
 echo Building NuSpec package...
 .\tools\NuPack.exe .\Sider.nuspec
 .\tools\7za.exe a -r sider.nupkg.zip sider.*.nupkg
+
+:end
