c8bfb2501957b942efab2bd932e679e1bfee2fb4
Chakrit Wichian <chakrit@2nitedesign.com>
2011-02-03 06:14:15 +0700

    Upped version to 0.3

--- SiderAssemblyInfo.cs
+++ SiderAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Sider.Tests")]
 
 #pragma warning disable 1607
-[assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.2.*")]
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.3.*")]
