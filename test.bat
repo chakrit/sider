597b9f0a08143de17db1ab1c562e5c033a191da8
Chakrit Wichian <chakrit@2nitedesign.com>
2011-04-07 05:21:00 +0700

    Make test.bat restart after hitting a key so we can do continuous test. .

--- test.bat
+++ test.bat
@@ -1,4 +1,12 @@
 @ECHO OFF
 
-msbuild Sider.sln /p:Configuration=Debug
-.\packages\NUnit.2.5.9.10348\Tools\nunit-console.exe Sider.Tests.nunit
+:loop
+echo Building...
+msbuild Sider.sln /p:Configuration=Debug > nul
+echo Running NUnit tests...
+.\packages\NUnit.2.5.9.10348\Tools\nunit-console.exe Sider.Tests.nunit /nologo %1 %2 %3 %4 %5 %6
+
+echo.
+echo "Press CTRL+C to stop."
+pause
+goto loop
