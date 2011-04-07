727f8e596e8b0ca7b002565b7c0919d1ddd54530
Chakrit Wichian <chakrit@2nitedesign.com>
2011-04-08 01:11:05 +0700

    Begin work on translators.

---
+++ src/Sider/ITranslator.cs
@@ -0,0 +1,15 @@
+﻿
+using System.IO;
+
+namespace Sider
+{
+  public interface ITranslator<T>
+  {
+    T Read(RedisSettings settings, Stream src, int length);
+
+    int GetBytesNeeded(RedisSettings settings);
+    void Write(byte[] buffer, int offset, int count);
+
+    void Reset();
+  }
+}
---
+++ src/Sider/StringTranslator.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO;
+
+namespace Sider
+{
+  public class StringTranslator : ITranslator<string>
+  {
+    public string Read(RedisSettings settings, Stream src, int length)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public int GetBytesNeeded(RedisSettings settings)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Write(byte[] buffer, int offset, int count)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Reset()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
