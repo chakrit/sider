ae26fe69e417a9e781dbc7d1915bbf7944e3f2b1
Chakrit Wichian <chakrit@2nitedesign.com>
2011-04-16 18:18:57 +0700

    Added 2 more exception types to differentiate between read exceptions and write exceptions.

---
+++ src/Sider/Exceptions.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Sider
+{
+  public sealed class WriteException : Exception
+  {
+    public WriteException(Exception innerException) :
+      base("Exception occurred while writing data to Redis.", innerException) { }
+  }
+
+  public class ReadException : Exception
+  {
+    public ReadException(Exception innerException) :
+      base("Exception occurred while reading data from Redis.", innerException) { }
+  }
+
+  public class ResponseException : Exception
+  {
+    public ResponseException(string msg) :
+      base(msg) { }
+
+    public ResponseException(string msg, Exception ex) :
+      base(msg, ex) { }
+  }
+}
