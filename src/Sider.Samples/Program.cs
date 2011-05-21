d9f4b6e73c4c6e739f36a711270e0aefc877d33e
Chakrit Wichian <chakrit@2nitedesign.com>
2011-05-21 12:08:46 +0700

    Minor style cleanup.

--- src/Sider.Samples/Program.cs
+++ src/Sider.Samples/Program.cs
@@ -1,6 +1,6 @@
 ﻿
-using System.Linq;
 using System;
+using System.Linq;
 
 namespace Sider.Samples
 {
--- src/Sider/MessageType.cs
+++ src/Sider/MessageType.cs
@@ -16,15 +16,15 @@ namespace Sider
   {
     public static MessageType Parse(string value)
     {
-    switch (value) {
-    case "message": return MessageType.Message;
-    case "pmessage": return MessageType.PMessage;
-    case "subscribe": return MessageType.Subscribe;
-    case "psubscribe": return MessageType.PSubscribe;
-    case "unsubscribe": return MessageType.Unsubscribe;
-    case "punsubscribe": return MessageType.PUnsubscribe;
+      switch (value) {
+      case "message": return MessageType.Message;
+      case "pmessage": return MessageType.PMessage;
+      case "subscribe": return MessageType.Subscribe;
+      case "psubscribe": return MessageType.PSubscribe;
+      case "unsubscribe": return MessageType.Unsubscribe;
+      case "punsubscribe": return MessageType.PUnsubscribe;
 
-    default: return MessageType.Unknown;
+      default: return MessageType.Unknown;
       }
     }
   }
