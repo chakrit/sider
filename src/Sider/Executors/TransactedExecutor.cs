c406e9c439e35ca6ed6aa016f16991c813421004
Chakrit Wichian <chakrit@oozou.com>
2011-12-17 01:01:56 +0700

    Attempt to pipeline MULTI/EXEC by default.

--- src/Sider/Executors/TransactedExecutor.cs
+++ src/Sider/Executors/TransactedExecutor.cs
@@ -11,10 +11,15 @@ namespace Sider.Executors
       if (previous is TransactedExecutor)
         throw new InvalidOperationException("Cannot nest MULTI/EXEC.");
 
-      // TODO: Make possible.
+      // TODO: Do a really thorough test that this won't randomly break\
+      // TODO: Maybe shouldn't really auto-pipeline by default because
+      //   some commands could throw an error and it would be hard to debug
+      //   if the error only happens when .Finish() (EXEC) is called.
+      //   This shouldn't be the case with manual piplining because the scope
+      //   is explicitly stated by the user.
       if (previous is PipelinedExecutor)
         throw new InvalidOperationException(
-          "MULTI/EXEC cannot be called inside .Pipeline()");
+          "MULTI/EXEC is automatically pipelined.");
 
       base.Init(previous);
     }
@@ -23,17 +28,20 @@ namespace Sider.Executors
     public override T Execute<T>(Invocation<T> invocation)
     {
       // TODO: Provide a settings to pipeline MultiExec automatically
-      var result = base.Execute(invocation); // invocation.Read should be queued
-      Writer.Flush(); // should flush? or wait for pipeline?
-      Reader.ReadQueued(); // read a +QUEUED
-
-      return result;
+      return base.Execute(invocation); // issue write and queue reads
     }
 
     public override IEnumerable<object> Finish()
     {
       Writer.WriteCmdStart("EXEC", 0);
       Writer.Flush();
+
+      // reads out an equal number of +QUEUEDs beforehand
+      // TODO: May need to restart if a read doesn't return an expected +QUEUEDs
+      //   which means *some* commands may have been improperly sent
+      //   may need to issue a DISCARD and start over.
+      for (int i = 0; i < ReadsQueue.Count; i++)
+        Reader.ReadQueued();
 
       Reader.ReadMultiBulkHeader(); // read-out a multi-bulk header
       return base.Finish(); // read out all the queued reads
