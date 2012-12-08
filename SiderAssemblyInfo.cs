5309176e9ad66ab7f1a5581dcb06052fd132c9e7
Chakrit Wichian <service@chakrit.net>
2012-12-09 04:53:13 +0700

    Update packages.
    Since it's been quite sometime.

--- SiderAssemblyInfo.cs
+++ SiderAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Sider")]
 [assembly: AssemblyDescription("SIDER : REDIS bindings for C#/.NET 4.0 -- Redis client that sticks to the metal. Minimal, lightweight, easy to use, no fancy APIs, no gigantic class hierarchies. No ORM madness. Assumes knowledge of how Redis works. -- Sider works with Redis 2.2+.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Chakrit Wichian (http://chakrit.net/)")]
+[assembly: AssemblyCompany("Chakrit Wichian <service@chakrit.net> (http://chakrit.net/)")]
 [assembly: AssemblyProduct("Sider")]
 
 [assembly: InternalsVisibleTo("Sider.Tests")]
