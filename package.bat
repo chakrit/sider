e88b3ebed0f24f97fd9f6035b1c69ff80d3a3bb1
bart <bart.gommers@isatis-health.nl>
2018-09-05 11:30:25 +0200

    package as netstandard package

--- package.bat
+++ package.bat
@@ -3,7 +3,8 @@
 
 echo Building NuGet package...
 mkdir build\packages
-.\.nuget\NuGet.exe pack .\src\Sider\Sider.csproj -build -o build\packages -sym -verbosity detailed -exclude Sider.GUI -exclude Sider.Benchmark
+
+dotnet pack .\src\Sider\Sider.csproj -o ..\..\build\packages\ --include-symbols -v d -c Release
 
 echo Packages built:
 dir build\packages /b/s
